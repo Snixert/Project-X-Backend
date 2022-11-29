68fa44ddb6ff9a68d818f896ed2ff60a0115f5bb
Jimmy Wolthers Nielsen <jwn@praktikcenter.lan>
2022-11-29 08:50:58 +0100

    add get single player method

--- ProjectXBackend/Controllers/PlayersController.cs
+++ ProjectXBackend/Controllers/PlayersController.cs
@@ -57,5 +57,51 @@ namespace ProjectXBackend.Controllers
                 }).ToListAsync();
             return Ok(players);
         }
+
+        [HttpGet]
+        [Route("{id:int}")]
+        public async Task<IActionResult> GetPlayer(int id)
+        {
+            var players = await dbContext.Players
+                .Where(p => p.Id == id)
+                .Select(p => new
+                {
+                    Id = p.Id,
+                    Name = p.Name,
+                    WeaponId = p.WeaponId,
+                    Level = p.Level,
+                    Currency = p.Currency,
+                    Weapon = new
+                    {
+                        Type = p.Weapon.Type,
+                        Name = p.Weapon.Name,
+                        ItemStats = p.Weapon.ItemStats.Select(w => new
+                        {
+                            StatsId = w.StatsId,
+                            StatName = w.Stats.StatName,
+                            StatsValue = w.StatsValue
+                        }),
+                    },
+                    PlayerStats = p.PlayerStats.Select(ps => new
+                    {
+                        StatsId = ps.StatsId,
+                        StatsValue = ps.StatsValue,
+                        StatName = ps.Stats.StatName
+                    }),
+                    Items = p.Items.Select(i => new
+                    {
+                        Id = i.Id,
+                        Type = i.Type,
+                        Name = i.Name,
+                        ItemStats = i.ItemStats.Select(f => new
+                        {
+                            StatsId = f.StatsId,
+                            StatName = f.Stats.StatName,
+                            StatValue = f.StatsValue
+                        })
+                    })
+                }).ToListAsync();
+            return Ok(players);
+        }
     }
 }
